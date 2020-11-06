fc1c9f30cf8229718f81d779b322a25edfcc072c
jubermomin323 <jubermomin323@gmail.com>
2020-11-07 01:15:21 +0530

    ui updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import AddQue from './components/AddQue/addQue';
 import Ques from './components/AddQue/ques';
 
 function App() {
   return (
     <div className="App">
-      {/* <AddQue /> */}
       <Ques />
     </div>
   );
--- src/components/AddQue/ques.js
+++ src/components/AddQue/ques.js
@@ -12,33 +12,45 @@ const Ques = () =>{
         //console.log(e.target.value)
         questions.text = e.target.value
     }
-    const addNext = (e) =>{
-        console.log(e.target.value)
+    const addOption = (e) =>{
+        //console.log(e.target.value)
         const tempArray = options
         tempArray[e.target.id] = e.target.value
         questions.options[e.target.id] = e.target.value
     }
     const newQue = () =>{
-
+        // var data = {...questions}
+        // console.log(data)
         document.getElementById("abc").value = ""
         for(let i=0; i<4; i++){
             document.getElementById(i).value = ""
         }
+    }
+    const addData = () =>{
+        fetch("",{
+            method: "post",
+            body: JSON.stringify({
+                questions
+            })
+        }, console.log("success"))
     }
 
 
     return(
         <div className="container">
 
-        <input type="text" id="abc" onChange={(e)=>addQues(e)}></input>
+        <input type="text" id="abc" onChange={(e)=>addQues(e)} placeholder="Add question here" />
         {
             [...Array(4)].map((val,index) => {
-                return <input type="text" key ={index} id={index} onChange={(e)=>addNext(e)}></input>
-                
+                return (
+                    <div className="options">
+                    <input type="text" key ={index} id={index} onChange={(e)=>addOption(e)} placeholder="Add option here" />
+                    </div>
+                    )
             })
         }
-        <button onClick={()=>console.log(questions)}>Submit</button>
-        <button className="btn" onClick={()=>newQue()} >Add Next Question</button>
+        <button className="btn"  onClick={()=>addData()}>Submit</button>
+        <button className="btn" style={{float:"right"}} onClick={()=>newQue()} >Add Next Question</button>
     </div>
     )
     
